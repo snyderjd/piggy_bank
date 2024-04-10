ff4294a2142fcc499038d48acee0452647bda83f
Joe Snyder <snyderjosephd@yahoo.com>
2024-04-09 22:16:34 -0500

    Add Ecto schemas for AccountType and Currency

---
+++ lib/piggy_bank/currencies/currency.ex
@@ -0,0 +1,28 @@
+defmodule PiggyBank.Currency do
+  @moduledoc """
+  Schema and relationships for currencies
+  """
+
+  use Ecto.Schema
+
+  alias Ecto.Changeset
+
+  @type t :: %__MODULE__{
+    name: String.t(),
+    code: String.t()
+  }
+
+  schema "currencies" do
+    field :name, :string
+    field :code, :string
+
+    timestamps()
+  end
+
+  @spec changeset(map(), map()) :: Changeset.t()
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> Changeset.cast(params, [:name, :code])
+    |> Changeset.validate_required([:name, :code])
+  end
+end
