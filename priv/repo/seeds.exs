00568fe133d2f7fb5c0750d0d16b4d2b4d15e1c2
Joe Snyder <snyderjosephd@yahoo.com>
2024-04-21 15:44:04 -0500

    Add initial sample transactions to seed script

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,16 +10,7 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-@doc """
-Need to insert some seed data to get up and running
-
-users
-currencies
-account_types
-accounts
-ledger_entries
-transactions
-"""
+alias PiggyBank.{Account, AccountType, Currency, LedgerEntry, Repo, Transaction, User}
 
 # Bcrypt.hash_pwd_salt("password123")
 # "$2b$12$ezayYzr2qcPUiGCXmhvjG.I6Y0Y1fP32WDY9xac4F5HzVq7daO4I2"
@@ -38,15 +29,15 @@ dwight_schrute = Repo.insert!(%User{
 
 # currencies
 usd = Repo.insert!(%Currency{name: "US Dollar", code: "USD"})
-eur = Repo.insert!(%Currency{name: "Euro", code: "EUR"})
-gpb = Repo.insert!(%Currency{name: "British Pound", code: "GBP"})
+_eur = Repo.insert!(%Currency{name: "Euro", code: "EUR"})
+_gpb = Repo.insert!(%Currency{name: "British Pound", code: "GBP"})
 
 # account_types
 asset = Repo.insert!(%AccountType{name: "Asset"})
 liability = Repo.insert!(%AccountType{name: "Liability"})
-equity = Repo.insert!(%AccountType{name: "Equity"})
+_equity = Repo.insert!(%AccountType{name: "Equity"})
 revenue = Repo.insert!(%AccountType{name: "Revenue"})
-expense = Repo.insert!(%AccountType{name: "Expense"})
+_expense = Repo.insert!(%AccountType{name: "Expense"})
 
 # accounts
 cash = Repo.insert!(%Account{name: "Cash", usd_current_balance: 0, account_type: asset})
@@ -71,9 +62,121 @@ fee_revenue = Repo.insert!(%Account{name: "Fee Revenue", usd_current_balance: 0,
 
 # transactions + ledger_entries
 # Warren Buffett deposits 1_000
-# Warren Buffett withdraws 250
-# Warren Buffett transfers 100 to Dwight Schrute
-# Charge $5 fee to Warren Buffett
-# Charge $5 fee to Dwight Schrute
+deposits_1000 = Repo.insert!(%LedgerEntry{
+  description: "debit cash 1000, credit Warren Buffett Checking 1000",
+  date: NaiveDateTime.new!(2024, 4, 12, 0, 0, 0)
+})
 
-# ledger_entries
+_debit_cash_1000 = Repo.insert!(%Transaction{
+  transaction_type: "debit",
+  amount: 1000.00,
+  date: NaiveDateTime.new!(2024, 4, 12, 0, 0, 0),
+  account: cash,
+  currency: usd,
+  ledger_entry: deposits_1000
+})
+
+_credit_checking_1000 = Repo.insert!(%Transaction{
+  transaction_type: "credit",
+  amount: 1000.00,
+  date: NaiveDateTime.new!(2024, 4, 12, 0, 0, 0),
+  account: warren_buffett_checking,
+  currency: usd,
+  ledger_entry: deposits_1000
+})
+
+# Warren Buffett withdraws 250
+withdraws_250 = Repo.insert!(%LedgerEntry{
+  description: "credit cash 250, debit Warren Buffett Checking 250",
+  date: NaiveDateTime.new!(2024, 4, 14, 0, 0, 0)
+})
+
+_credit_cash_250 = Repo.insert!(%Transaction{
+  transaction_type: "credit",
+  amount: 250.00,
+  date: NaiveDateTime.new!(2024, 4, 14, 0, 0, 0),
+  account: cash,
+  currency: usd,
+  ledger_entry: withdraws_250
+})
+
+_debit_checking_250 = Repo.insert!(%Transaction{
+  transaction_type: "debit",
+  amount: 250.00,
+  date: NaiveDateTime.new!(2024, 4, 14, 0, 0, 0),
+  account: warren_buffett_checking,
+  currency: usd,
+  ledger_entry: withdraws_250
+})
+
+# Warren Buffett transfers 100 to Dwight Schrute
+transfers_100 = Repo.insert!(%LedgerEntry{
+  description: "debit Warren Buffett Checking 100, credit Dwight Schrute Checking 100",
+  date: NaiveDateTime.new!(2024, 4, 15, 0, 0, 0)
+})
+
+_debit_wb_100 = Repo.insert!(%Transaction{
+  transaction_type: "debit",
+  amount: 100.00,
+  date: NaiveDateTime.new!(2024, 4, 15, 0, 0, 0),
+  account: warren_buffett_checking,
+  currency: usd,
+  ledger_entry: transfers_100
+})
+
+_credit_ds_100 = Repo.insert!(%Transaction{
+  transaction_type: "credit",
+  amount: 100.00,
+  date: NaiveDateTime.new!(2024, 4, 15, 0, 0, 0),
+  account: dwight_schrute_checking,
+  currency: usd,
+  ledger_entry: transfers_100
+})
+
+# Charge $5 fee to Warren Buffett
+wb_fee_5 = Repo.insert!(%LedgerEntry{
+  description: "credit Fee Revenue 5, debit Warren Buffett Checking 5",
+  date: NaiveDateTime.new!(2024, 4, 18, 0, 0, 0)
+})
+
+_credit_fee_revenue_5_wb = Repo.insert!(%Transaction{
+  transaction_type: "credit",
+  amount: 5.00,
+  date: NaiveDateTime.new!(2024, 4, 18, 0, 0, 0),
+  account: fee_revenue,
+  currency: usd,
+  ledger_entry: wb_fee_5
+})
+
+_debit_wb_5 = Repo.insert!(%Transaction{
+  transaction_type: "debit",
+  amount: 5.00,
+  date: NaiveDateTime.new!(2024, 4, 18, 0, 0, 0),
+  account: warren_buffett_checking,
+  currency: usd,
+  ledger_entry: wb_fee_5
+})
+
+# Charge $5 fee to Dwight Schrute
+ds_fee_5 = Repo.insert!(%LedgerEntry{
+  description: "credit Fee Revenue 5, debit Dwight Schrute Checking 5",
+  date: NaiveDateTime.new!(2024, 4, 18, 0, 0, 0)
+})
+
+_credit_fee_revenue_5_ds = Repo.insert!(%Transaction{
+  transaction_type: "credit",
+  amount: 5.00,
+  date: NaiveDateTime.new!(2024, 4, 18, 0, 0, 0),
+  account: fee_revenue,
+  currency: usd,
+  ledger_entry: ds_fee_5
+})
+
+_debit_ds_5 = Repo.insert!(%Transaction{
+  transaction_type: "debit",
+  amount: 5.00,
+  date: NaiveDateTime.new!(2024, 4, 18, 0, 0, 0),
+  account: dwight_schrute_checking,
+  currency: usd,
+  ledger_entry: ds_fee_5
+})
